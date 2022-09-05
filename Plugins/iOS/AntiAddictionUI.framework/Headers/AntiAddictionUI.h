88556c6b874784d739f6f5c3f08c934f10fe461a
ci-gitlab <tds-developer@xd.com>
2022-09-05 16:43:10 +0800

    feat:update upm

---
+++ Plugins/iOS/AntiAddictionUI.framework/Headers/AntiAddictionUI.h
@@ -0,0 +1,18 @@
+//
+//  AntiAddictionUI.h
+//  AntiAddictionUI
+//
+//  Created by jessy on 2021/9/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AntiAddictionUI.
+FOUNDATION_EXPORT double AntiAddictionUIVersionNumber;
+
+//! Project version string for AntiAddictionUI.
+FOUNDATION_EXPORT const unsigned char AntiAddictionUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AntiAddictionUI/PublicHeader.h>
+
+
