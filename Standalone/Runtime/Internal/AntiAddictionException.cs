0118dc44b0cf151dbc67db0431fde70550b1aef9
ci-gitlab <tds-developer@xd.com>
2025-12-24 15:45:26 +0800

    feat:update upm

---
+++ Standalone/Runtime/Internal/AntiAddictionException.cs
@@ -0,0 +1,24 @@
+﻿using TapTap.Common;
+
+namespace TapTap.AntiAddiction 
+{
+    public class AntiAddictionException : TapException 
+    {
+        public string Error { get; internal set; }
+
+        public string Description { get; internal set; }
+
+        public long Now { get; internal set; }
+
+        public long ErrorCode {get; internal set;}
+
+        public string ErrorMessage  { get; internal set; }
+
+        public AntiAddictionException(int code, string message) : base(code, message) { }
+
+        public bool IsTokenExpired(){
+            return Error != null && Error.Equals("business_code_error") && ErrorCode == 200000;
+        }
+
+    }
+}
